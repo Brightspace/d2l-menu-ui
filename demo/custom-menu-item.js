66ebc243ca642b100db17e3e1e46b1ae1462fcf0
Margaree Peacocke <margaree.p@gmail.com>
2020-01-23 13:55:08 -0500

    use old custom-menu-item, import separator

--- demo/custom-menu-item.js
+++ demo/custom-menu-item.js
@@ -1,44 +1,40 @@
-import { css, html, LitElement } from 'lit-element/lit-element.js';
-import { MenuItemMixin } from '@brightspace-ui/core/components/menu/menu-item-mixin.js';
-import { menuItemStyles } from '@brightspace-ui/core/components/menu/menu-item-styles.js';
+import '@polymer/polymer/polymer-legacy.js';
+import '../d2l-menu-item-styles.js';
+import '../d2l-menu-item-behavior.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+const $_documentContainer = document.createElement('template');
 
-class CustomMenuItem extends MenuItemMixin(LitElement) {
+$_documentContainer.innerHTML = `<dom-module id="d2l-custom-menu-item">
+	<template strip-whitespace="">
+		<style include="d2l-menu-item-styles">
+			:host {
+				display: block;
+				padding: 0.75rem 1.5rem;
+			}
+			:host span {
+				line-height: 1rem;
+				overflow-x: hidden;
+				text-overflow: ellipsis;
+				white-space: nowrap;
+			}
+			:host(:hover) span,
+			:host(:focus) span {
+				display: inline-block;
+				-webkit-transform: rotateY(360deg);
+				transform: rotateY(360deg);
+				transition: transform 2s;
+			}
+		</style>
+		<span>[[text]]</span>
+		<slot></slot>
+	</template>
 
-	static get properties() {
-		return {
-			text: { type: String }
-		};
-	}
+</dom-module>`;
 
-	static get styles() {
-		return [ menuItemStyles,
-			css`
-				:host {
-					padding: 0.75rem 1.5rem;
-				}
-				:host span {
-					line-height: 1rem;
-					overflow-x: hidden;
-					text-overflow: ellipsis;
-					white-space: nowrap;
-				}
-				:host(:hover) span,
-				:host(:focus) span {
-					display: inline-block;
-					-webkit-transform: rotateY(360deg);
-					transform: rotateY(360deg);
-					transition: transform 2s;
-				}
-			`
-		];
-	}
-
-	render() {
-		return html`
-			<span>${this.text}</span>
-			<slot></slot>
-		`;
-	}
-}
-
-customElements.define('d2l-custom-menu-item', CustomMenuItem);
+document.head.appendChild($_documentContainer.content);
+Polymer({
+	is: 'd2l-custom-menu-item',
+	behaviors: [
+		D2L.PolymerBehaviors.MenuItemBehavior
+	]
+});
